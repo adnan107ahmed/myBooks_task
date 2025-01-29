fe039cca5b1c67859bfe32983bc23b9b3d66986c
adnan107ahmed <adnan107ahmed@gmail.com>
2025-01-29 02:15:52 +0000

    First Commit

--- ebooks/urls.py
+++ ebooks/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('create-book/', views.create_or_update_book, name='create_book'),
+    path('update-book/<int:book_id>/', views.create_or_update_book, name='update_book'),
 ]
--- ebooks/views.py
+++ ebooks/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
 from .models import  myBooks
 
-# Create your views here.
 def home(request):
     books=myBooks.objects.all()
     context={'books':books}
     return render (request, 'index.html', context)
+
+def delete_book(request, book_id):
+    book = get_object_or_404(myBooks, id=book_id)  
+    book.delete()
+    return redirect('home')
+
+def create_or_update_book(request, book_id=None):
+    if book_id:  
+        book = get_object_or_404(myBooks, id=book_id)
+    else:  
+        book = None
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        genre = request.POST.get('genre')
+        price = request.POST.get('price')
+        stock = request.POST.get('stock')
+
+        if book:  
+            book.title = title
+            book.author = author
+            book.genre = genre
+            book.price = price
+            book.stock = stock
+            book.save()
+        else:  
+            myBooks.objects.create(
+                title=title,
+                author=author,
+                genre=genre,
+                price=price,
+                stock=stock
+            )
+        return redirect('home')
+    return render(request, 'form.html', {'book': book})
